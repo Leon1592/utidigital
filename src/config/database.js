c9093af03f122cc2038d0ae329eb32581f55e3a8
joaopedro <devjoaopedrofepereira2009@gmail.com>
2026-04-15 16:21:13 -0300

    alt

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,23 @@
-const { pool } = require('./db');
+//"npm install pg" para funcionar o PostgreSQL no Node.js
+
+const { Pool } = require("pg")
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'utidigital',
-    password: '1234',
+    user: "postgres",
+    host: "192.168.1.139",
+    database: "dbutidigital",
+    password: "dbutidigital1234#",
     port: 5432
 })
+
+//teste de conexão com o banco de dados
+pool.connect()
+    .then(client => {
+        console.log("Conectado com o banco de dados PostgreSQL")
+        client.release()
+    })
+    .catch(err => {
+        console.error("Erro ao conectar com o banco: ", err)
+    })
 
 module.exports = pool
