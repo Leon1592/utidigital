c683aa96c878b22d7be3fd00c2fe18373ab7cc4d
joaopedro <devjoaopedrofepereira2009@gmail.com>
2026-05-01 00:50:05 -0300

    autenticação postgresql com a api

--- src/lib/auth.ts
+++
@@ -1,7 +0,0 @@
-import { betterAuth } from "better-auth";
-import { kyselyAdapter } from "@better-auth/kysely-adapter";
-import db from "./db";
-
-export const auth = betterAuth({
-  database: kyselyAdapter(db),
-});
